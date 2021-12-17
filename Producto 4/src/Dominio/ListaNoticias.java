8973431fb6e7206aacd1baf7098a5c781d773bf6
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2021-12-16 23:37:53 -0400

    Create ListaNoticias.java

---
+++ Producto 4/src/Dominio/ListaNoticias.java
@@ -0,0 +1,15 @@
+package Dominio;
+
+import java.util.ArrayList;
+
+public class ListaNoticias {
+	private ArrayList<Noticia> noticias;
+	
+	public ListaNoticias() {
+		noticias = new ArrayList<Noticia>();
+	}
+	
+	public ArrayList<Noticia> getNoticias(){
+		return noticias;
+	}
+}
