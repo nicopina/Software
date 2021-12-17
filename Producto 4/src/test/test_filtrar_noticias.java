49311268ce606e239f48111d43c5ec8482edb827
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2021-12-17 11:53:52 -0400

    inicios test

---
+++ Producto 4/src/test/test_filtrar_noticias.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test_filtrar_noticias {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
