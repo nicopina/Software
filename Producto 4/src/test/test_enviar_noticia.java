76e2ee86afb8b9842fe4736e72886f8de67911df
nicopina <64911016+nicopina@users.noreply.github.com>
2021-12-17 13:21:17 -0300

    Create test_enviar_noticia.java

---
+++ Producto 4/src/test/test_enviar_noticia.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import Dominio.Noticia;
+import Logica.SistemaNoticiasImpl;
+
+public class test_enviar_noticia {
+	
+	private  SistemaNoticiasImpl enviarNoticia = new SistemaNoticiasImpl();
+	
+	private 
+	
+
+}
