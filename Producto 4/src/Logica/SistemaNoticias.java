f6765f25769a6392d7be78761ce1a1113fe8fb68
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2021-12-17 00:27:15 -0400

    interfaz con ver noticia

--- Producto 4/src/Logica/SistemaNoticias.java
+++ Producto 4/src/Logica/SistemaNoticias.java
@@ -2,9 +2,12 @@ package Logica;
 
 import java.util.Date;
 
+import Dominio.*;
+
 public interface SistemaNoticias {
 	
 	public String ordenarNoticia(String [] lista,Date fecha,String tematica);
 	
+	public void verNoticias(ListaNoticias noticias);
 
 }
