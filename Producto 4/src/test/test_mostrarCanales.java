96397693eba749d554f92683cff3f650fb02d20b
nicopina <64911016+nicopina@users.noreply.github.com>
2021-12-17 13:47:41 -0300

    test canales

--- Producto 4/src/test/test_mostrarCanales.java
+++ Producto 4/src/test/test_mostrarCanales.java
@@ -1,5 +1,19 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import Dominio.Canal;
+import Logica.SistemaNoticiasImpl;
+
+
 public class test_mostrarCanales {
+	
+	private SistemaNoticiasImpl implement = new SistemaNoticiasImpl();
+	private ArrayList<Canal> listaCanalesPrueba = new ArrayList<Canal>();
+	
+	mostrarCanales();
+	
 
 }
