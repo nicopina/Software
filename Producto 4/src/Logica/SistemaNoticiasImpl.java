64df851e95977cc534b07665dab3707995aedcc9
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2021-12-17 00:51:44 -0400

    ver noticias primer avance

--- Producto 4/src/Logica/SistemaNoticiasImpl.java
+++ Producto 4/src/Logica/SistemaNoticiasImpl.java
@@ -1,5 +1,56 @@
 package Logica;
 
-public class SistemaNoticiasImpl {
+import java.util.ArrayList;
 
+import Dominio.ListaNoticias;
+import Dominio.Noticia;
+
+public class SistemaNoticiasImpl implements SistemaNoticias{
+	
+	public SistemaNoticiasImpl() {
+		
+	}
+	
+	public void verNoticias(ListaNoticias noticias) {
+		ArrayList<Noticia> listaNoticias = noticias.getNoticias();
+		System.out.println("------Ver noticias-----");
+		for (int i = 0; i < listaNoticias.size(); i++) {
+			Noticia noticiaI = listaNoticias.get(i);
+			System.out.println(i + " "+ noticiaI.getTitulo());
+		}
+		
+		System.out.println("Ingrese la opción de la operación que desee realizar:\n");
+		System.out.println("X. Ordenar cronológicamente\nY. Filtrar por tema");
+		String opcionEntrada = ""; //Falta scanner
+		
+		if (opcionEntrada.equalsIgnoreCase("x")) {
+			//ordenar
+		}
+		
+		else {
+			if (opcionEntrada.equalsIgnoreCase("y")) {
+				//filtrar tema
+			}
+			
+			else {
+				//Vemos si es que la entrada es un índice válido de alguna noticia.
+				try {
+					int indice = Integer.parseInt(opcionEntrada);
+					if (indice >= 0 && indice < listaNoticias.size()) {
+						System.out.println("Abriendo noticia...");
+						Noticia noticiaSeleccionada = listaNoticias.get(indice);
+						System.out.println(noticiaSeleccionada.getTitulo()+"\n");
+						System.out.println(noticiaSeleccionada.getTexto());
+					}
+					else {
+						System.out.println("Opción inválida"); 
+					}
+				}
+				
+				catch(Exception e){
+					System.out.println("Opción inválida"); 
+				}
+			}
+		}
+	}
 }
